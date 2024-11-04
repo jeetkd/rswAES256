a20a2b6b6c6ecee53b2fadca27882602a134c163
Cat Hanbit <chilmanpyo@gmail.com>
2024-11-04 17:44:21 +0900

    feat: Add AES-128 CBC encryption module (encrypt.go, EncryptAES)

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"rswAES256/config"
+	"rswAES256/encrypt"
 )
 
 var path = "./config.toml"
@@ -12,8 +14,17 @@ var pathFlag = flag.String("config", path, "set toml path")
 
 func main() {
 	flag.Parse()
+	data := []byte("1234567890123456") // 128비트
+	key := []byte("0123456789abcdef")  // 128비트
 
 	// 설정파일 설정.
 	c := config.NewConfig(path)
 	fmt.Println(c)
+
+	// AES 암호화
+	cipherText, err := encrypt.EncryptAES(data, key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("cipherText : %x\n", cipherText)
 }
