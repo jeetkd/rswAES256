29998baa3db13e11ae8390197e6f8fa77a5d3638
jeetkd <chilmanpyo@gmail.com>
2024-11-28 15:31:55 +0900

    refactor(main): change path for .exe file in real test  On branch main  Changes to be committed: 	modified:   display/main.go

--- display/main.go
+++ display/main.go
@@ -1,41 +1,1 @@
-package main
-
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"rswAES256/client"
-	"rswAES256/config"
-	"strings"
-)
-
-func main() {
-	fmt.Println("Put correct key!!!. Files can not back to original files when you put wrong key!!!")
-	fmt.Println("검증되지 않은 키를 입력 시 파일을 되돌릴 수 없습니다!!! 올바른 키를 입력하세요!!!")
-	for {
-		fmt.Print("Put key for decryption your files : ")
-
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		input := strings.TrimSpace(scanner.Text()) // 앞뒤 공백 제거
-		if input == "" {
-			fmt.Println("Error: 입력값이 비어있습니다!")
-		} else {
-
-			// 설정파일 설정.
-			c := config.NewConfig("./config.toml")
-			fmt.Println(c)
-
-			// 입력한 키를 client에 넣어줌.
-			newClient := client.NewClient(c)
-			newClient.Key = input
-
-			//복호화 진행
-			if err := newClient.AESDecryptDirectory("./../cmd/test"); err != nil {
-				fmt.Println("failed to decrypt(복호화 실패) : ", err)
-			}
-
-			break
-		}
-	}
-}
+package mainimport (	"bufio"	"fmt"	"os"	"rswAES256/client"	"rswAES256/config"	"strings")func main() {	fmt.Println("Put correct key!!!. Files can not back to original files when you put wrong key!!!")	fmt.Println("검증되지 않은 키를 입력 시 파일을 되돌릴 수 없습니다!!! 올바른 키를 입력하세요!!!")	for {		fmt.Print("Put key for decryption your files : ")		scanner := bufio.NewScanner(os.Stdin)		scanner.Scan()		input := strings.TrimSpace(scanner.Text()) // 앞뒤 공백 제거		if input == "" {			fmt.Println("Error: 입력값이 비어있습니다!")		} else {			// 설정파일 설정.			c := config.NewConfig("./config.toml")			fmt.Println(c)			// 입력한 키를 client에 넣어줌.			newClient := client.NewClient(c)			newClient.Key = input			//복호화 진행			if err := newClient.AESDecryptDirectory("./test"); err != nil {				fmt.Println("failed to decrypt(복호화 실패) : ", err)			}			break		}	}}
